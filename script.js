49f4f5651f939403bfd7031b20399449c7fedaa2
Sospeter Mong'are <sosmongare@gmail.com>
2021-04-30 11:59:17 +0300

    Add files via upload
    First migration of the website from msnmongare.github.io

---
+++ script.js
@@ -0,0 +1,11 @@
+//Sidenav
+const sideNav = document.querySelector(".sidenav");
+M.Sidenav.init(sideNav, {}); //initializing our SideNav
+
+//Carousel
+const carousel = document.querySelector(".carousel");
+M.Carousel.init(carousel, {});
+
+//Scrollspy
+const scrollspy = document.querySelectorAll(".scrollspy");
+M.ScrollSpy.init(scrollspy, {});
